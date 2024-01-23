12e327a5f9eb0e5709dd2fdfd2c7242233aac4ea
Hols <samettcaner@gmail.com>
2024-01-23 22:11:11 +0300

    230124v2

--- middlewares/roleMiddleware.js
+++ middlewares/roleMiddleware.js
@@ -1,7 +1,7 @@
 module.exports = (roles) => {
   return (req, res, next) => {
     const userRole = req.body.role;
-    if (roles.includes()) {
+    if (roles.includes(userRole)) {
       next();
     } else {
       return res.status(401).send('You Cant!');
